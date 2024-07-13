14593e4142cc3ecc3a3f136f4a54517a4e62264b
Chandrajeetyadav130 <chandrajeetyadav130@gmail.com>
2024-07-14 00:48:17 +0530

    change env file

--- index.js
+++ index.js
@@ -2,22 +2,19 @@ const app= require("./app")
 const mongoose=require("mongoose")
 const dotenv=require("dotenv")
 const cloudinary=require("cloudinary")
-dotenv.config({path:"./config/config.env"})
+
+dotenv.config()
 const port=process.env.PORT
 const server=app.listen(port,()=>{
     console.log(`Server is listen at ${port}`)
 })
-const dbUrl=process.env.mongodbUrl
+const dbUrl=process.env.mongodbUrl1
 cloudinary.config({
     cloud_name:process.env.CLOUDINARY_NAME,
     api_key:process.env.CLOUDINARY_API_KEY,
     api_secret:process.env.API_SECRET_KEY
 })
- mongoose.connect(dbUrl,{
-  
-    // useNewUrlParser:true,
-    // useUnifiedTopology:true
-    }).then((val)=>{
+ mongoose.connect(dbUrl).then((val)=>{
         console.log("connection ok")
     }).catch((error)=>{
         console.log(error)
